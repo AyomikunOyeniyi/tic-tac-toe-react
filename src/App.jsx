a3af10d15f149ecc710f804bb3bc81bee6fb381b
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-03-28 22:22:42 -0400

    Enable users to add symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Player from "./components/Player"
+import { useState } from "react";
+
+import Player from "./components/Player";
+import GameBoard from "./components/Gameboard";
+
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleActivePlayer() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name={'Ayo'} symbol={'X'}></Player>
-          <Player name={'Jenna'} symbol={'O'}></Player>
+        <ol id="players" className="highlight-player">
+          <Player name={'Player 1'} symbol={'X'} isActive={activePlayer === 'X'}></Player>
+          <Player name={'Player 2'} symbol={'O'} isActive={activePlayer === 'O'}></Player>
         </ol>
-
-        GAME BOARD
+        <GameBoard onSelectSquare={handleActivePlayer} activePlayerSymbol={activePlayer}></GameBoard>
       </div>
 
       LOG
