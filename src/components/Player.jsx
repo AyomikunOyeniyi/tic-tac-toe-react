b27321de2a574657f4e41bb7f978b568b6ec1262
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-04-04 23:14:58 -0400

    Add new prop for name changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react"
 
-export default function Player({name, symbol, isActive}) {
+export default function Player({name, symbol, isActive, onNameChange}) {
     const [playerName, setPlayerName] = useState(name)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleNameChange(event) {
         setPlayerName(event.target.value);
     }
+
+    function handleEditClick() {
+        setIsEditing((editing) => !editing); 
+
+        if (isEditing) {
+            onNameChange(symbol, playerName);
+        }
+    }
+    
     return (
         <li className={isActive ? 'active' : undefined}>
             <span id="player">
               {!isEditing?<span className="player-name">{playerName}</span> : <input type="text" value={playerName} required onChange={handleNameChange}/>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={() => setIsEditing((editing) => !editing )}>{isEditing ? 'Save' : 'Edit'}</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
